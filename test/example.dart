b0766e5520661919a774f9a93c95028c0b1f9eaa
Leegijae <lgj010712@daelim.ac.kr>
2025-03-27 12:42:46 +0900

    커밋

---
+++ test/example.dart
@@ -0,0 +1,50 @@
+/* void main() {
+  String name = 'dart';
+  int age = 20;
+  double years = 2025;
+  bool isStudy = true;
+
+  var name2 = 'dart2';
+} */
+
+/* void main() {
+  dynamic name = 'dart';
+
+  name = 0;
+
+  print(name);
+} */
+
+/* void main() {
+
+  const API_URL = 'https://api.example.com';
+
+  var now = DateTime.now();
+
+  print(now);
+
+  now = DateTime.now();
+} */
+
+/* void main() {
+  final testClass = ClassExample();
+
+  final myAge = testClass.age();
+
+  print('나의 나이는 $myAge 입니다.');
+
+  final myBirth = testClass.birth(year: 2001, month: 7, day: 12);
+
+  print(myBirth);
+}
+
+class ClassExample {
+  String birth({required int year, required int month, int? day}) {
+    return '$year 년 $month 월 $day 일 입니다.';
+  }
+
+  int age({int birthYear = 2024}) {
+    return 2025 - birthYear;
+  }
+} */
+
