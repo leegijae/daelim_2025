0cfd1ec7a71c64b15549d7f86f502a455d45482d
Leegijae <lgj010712@daelim.ac.kr>
2025-05-08 11:52:13 +0900

    new

---
+++ news_portal/lib/app/router/app_router.dart
@@ -0,0 +1,37 @@
+import 'package:go_router/go_router.dart';
+import 'package:news_portal/presentation/login/login_screen.dart';
+import 'package:news_portal/presentation/news_list/news_list_screen.dart';
+
+final appRouter = GoRouter(
+  routes: [
+   GoRoute(
+    path: AppRoute.login.path, 
+    name: AppRoute.login.name, 
+    builder: (context, state) => LoginScreen(),),
+  ),
+  GoRoute(
+      path: AppRoute.newsList.path,
+      name: AppRoute.newsList.name,
+      pageBuilder: (context, state) {
+        return CustomTransitionPage(
+          key: state.pageKey,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: CurveTween(
+                curve: Curves.easeInOutCirc,
+              ).animate(animation), 
+              child: child,
+            );
+          },
+        );
+      ],
+      },
+      
+    ),      
+    
+
+enum AppRoute { login, newsList }
+
+extension AppRouteExtension on AppRoute {
+  String get path => '/$this';
+}
