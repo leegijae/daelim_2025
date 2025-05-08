0a56ae764eced11f43ab84dfb7d05664f663f793
Leegijae <lgj010712@daelim.ac.kr>
2025-05-08 12:00:07 +0900

    git

--- news_portal/lib/app/router/app_router.dart
+++ news_portal/lib/app/router/app_router.dart
@@ -1,15 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:news_portal/presentation/login/login_screen.dart';
 import 'package:news_portal/presentation/news_list/news_list_screen.dart';
 
 final appRouter = GoRouter(
+  initialLocation: AppRoute.login.path,
   routes: [
-   GoRoute(
-    path: AppRoute.login.path, 
-    name: AppRoute.login.name, 
-    builder: (context, state) => LoginScreen(),),
-  ),
-  GoRoute(
+    GoRoute(
+      path: AppRoute.login.path,
+      name: AppRoute.login.name,
+      builder: (context, state) => LoginScreen(),
+    ),
+    GoRoute(
       path: AppRoute.newsList.path,
       name: AppRoute.newsList.name,
       pageBuilder: (context, state) {
@@ -19,16 +21,16 @@ final appRouter = GoRouter(
             return FadeTransition(
               opacity: CurveTween(
                 curve: Curves.easeInOutCirc,
-              ).animate(animation), 
+              ).animate(animation),
               child: child,
             );
           },
+          child: NewsListScreen(),
         );
-      ],
       },
-      
-    ),      
-    
+    ),
+  ],
+);
 
 enum AppRoute { login, newsList }
 
